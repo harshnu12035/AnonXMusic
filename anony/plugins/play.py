2a3948f76ea3aee2b85da96cedc3b000e1b2a11a
harshnu12035 <harshnu12035@gmail.com>
2026-06-10 00:01:35 +0530

    Update play.py
    Signed-off-by: harshnu12035 <harshnu12035@gmail.com>

--- anony/plugins/play.py
+++ anony/plugins/play.py
@@ -49,25 +49,50 @@ async def play_hndlr(
         file = await tg.process_m3u8(url, sent.id, video)
 
     elif url:
-        if "playlist" in url:
-            await sent.edit_text(m.lang["playlist_fetch"])
-            tracks = await yt.playlist(
-                config.PLAYLIST_LIMIT, mention, url, video
-            )
+    if "playlist" in url:
+        await sent.edit_text(m.lang["playlist_fetch"])
+        tracks = await yt.playlist(
+            config.PLAYLIST_LIMIT, mention, url, video
+        )
 
-            if not tracks:
-                return await sent.edit_text(m.lang["playlist_error"])
+        if not tracks:
+            return await sent.edit_text(m.lang["playlist_error"])
 
-            file = tracks[0]
-            tracks.remove(file)
-            file.message_id = sent.id
+        file = tracks[0]
+        tracks.remove(file)
+        file.message_id = sent.id
+
+    else:
+        # Direct YouTube link support
+        if yt.valid(url):
+            match = yt.regex.match(url)
+
+            if match:
+                video_id = match.group(5)
+
+                # Video metadata fetch
+                file = await yt.search(
+                    f"https://www.youtube.com/watch?v={video_id}",
+                    sent.id,
+                    video=video
+                )
+
+            else:
+                file = None
+
         else:
-            file = await yt.search(url, sent.id, video=video)
-
-        if not file:
-            return await sent.edit_text(
-                m.lang["play_not_found"].format(config.SUPPORT_CHAT)
+            file = await yt.search(
+                url,
+                sent.id,
+                video=video
             )
+
+    if not file:
+        return await sent.edit_text(
+            m.lang["play_not_found"].format(
+                config.SUPPORT_CHAT
+            )
+        )
 
     elif len(m.command) >= 2:
         query = " ".join(m.command[1:])
